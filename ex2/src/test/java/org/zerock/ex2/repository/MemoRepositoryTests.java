e47601aaf18960f60ad3ab45bd8c66bd7183eaff
moonhwan123 <58998949+moonhwan123@users.noreply.github.com>
2021-06-21 20:59:53 +0900

    0621

--- ex2/src/test/java/org/zerock/ex2/repository/MemoRepositoryTests.java
+++ ex2/src/test/java/org/zerock/ex2/repository/MemoRepositoryTests.java
@@ -3,9 +3,15 @@ package org.zerock.ex2.repository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.test.annotation.Commit;
 import org.zerock.ex2.entity.Memo;
 
 import javax.transaction.Transactional;
+import java.util.List;
 import java.util.Optional;
 import java.util.stream.IntStream;
 
@@ -44,10 +50,10 @@ public class MemoRepositoryTests {
     public void testSelect2(){
         Long mno = 100L;
 
-        Memo memo = memoRepository.getOne(mno);
+//        Memo memo = memoRepository.getOne(mno);
 
         System.out.println("=====================================================================");
-        System.out.println(memo);
+ //       System.out.println(memo);
 
     }
 
@@ -70,5 +76,66 @@ public class MemoRepositoryTests {
         Long mno = 100L;
 
         memoRepository.deleteById(mno);
+    }
+
+    @Test
+    public void testPageDefault(){
+
+        Pageable pageable = PageRequest.of(0,10);
+
+        Page<Memo> result = memoRepository.findAll(pageable);
+        System.out.println(result);
+        System.out.println("-----------------------------------------------------");
+        System.out.println("Total pages : " + result.getTotalPages());
+        System.out.println("Total count : " + result.getTotalElements());
+        System.out.println("Page number : " + result.getNumber());
+        System.out.println("Page size : " + result.getSize());
+        System.out.println("has next page : " + result.hasNext());
+        System.out.println("first page : " + result.isFirst());
+        System.out.println("-----------------------------------------------------");
+        for(Memo memo : result.getContent()){
+            System.out.println(memo);
+        }
+    }
+
+    @Test
+    public void testSort(){
+        Sort sort1 = Sort.by("mno").descending();
+
+        //정렬조건 추가 하고 싶은경우
+        Sort sort2 = Sort.by("memoText").ascending();
+        Sort sortAll = sort1.and(sort2);
+
+        Pageable pageable = PageRequest.of(0,10,sortAll);
+
+        Page<Memo> result = memoRepository.findAll(pageable);
+
+        result.get().forEach(memo -> {
+            System.out.println(memo);
+        });
+    }
+
+    @Test
+    public void testQeuryMethods(){
+        List<Memo> list = memoRepository.findByMnoBetweenOrderByMnoDesc(70L,80L);
+        for(Memo memo : list){
+            System.out.println(memo);
+        }
+    }
+
+    @Test
+    public void testQeuryMethodWithPagable(){
+        Pageable pageable = PageRequest.of(0,10 , Sort.by("mno").descending());
+        Page<Memo> result = memoRepository.findByMnoBetween(10L,50L,pageable);
+        result.get().forEach(memo -> {
+            System.out.println(memo);
+        });
+    }
+
+    @Test
+    @Commit
+    @Transactional
+    public void testDeleteQueryMethods(){
+        memoRepository.deleteMemoByMnoLessThan(10L);
     }
 }
