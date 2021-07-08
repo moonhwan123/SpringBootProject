a373445c49cb006bbfa8027b471ddc3a55329ffe
moonhwan123 <58998949+moonhwan123@users.noreply.github.com>
2021-07-08 20:23:42 +0900

    0708

---
+++ guestbook/src/main/java/com/example/guestbook/entity/BaseEntity.java
@@ -0,0 +1,28 @@
+package com.example.guestbook.entity;
+
+import lombok.Getter;
+import org.apache.tomcat.jni.Local;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(value={AuditingEntityListener.class})
+abstract public class BaseEntity {
+
+    @CreatedDate
+    @Column(name="regdate" , updatable = false)
+    private LocalDateTime regDate;
+
+    @LastModifiedDate
+    @Column(name="moddate")
+    private LocalDateTime modDate;
+
+}
